5827bbbd1a34171b7d905cad6db3ece1bdb61002
Dmitry <arhidiman@mail.ru>
2024-06-27 22:53:06 +0700

    feat: integration with authentication servicd

--- index.ts
+++ index.ts
@@ -1,22 +1,46 @@
 const express = require('express')
-const cors =require('cors')
+import {Request, Response} from "express";
+import axios, {AxiosResponse} from 'axios'
+const request = require('request')
+
+// import request from 'request'
+const cors = require('cors')
 import goodsRouter from "./router/goodsRouter";
 import usersRouter from './router/usersRouter';
 //
 const app = express();
-const PORT = 5000;
+const MAIN_PORT = 5000;
+const AUTH_PORT = 6000;
 // //
 app.use(express.json());
 app.use(cors());
-// app.use('/api', goodsRouter);
+app.use('/api', goodsRouter);
 app.use('/api', usersRouter);
+
+const BASE_URL = 'http://localhost'
+
+
+app.post('/reg', async (req: Request, res: Response) => {
+    try {
+        const data = req.body
+        console.log(data, 'request from client')
+        console.log(`${BASE_URL}:${AUTH_PORT}/reg`)
+        
+        const authRes: AxiosResponse = await axios.post(`${BASE_URL}:${AUTH_PORT}/reg`, data)
+        const authData = authRes.data
+        console.log(authData, 'response from auth service')
+        res.status(201).send('response from auth server completed successfully')
+    } catch (err: any) {
+        console.log(err.message)
+    }
+})
 
 const startApp = async () => {
     try {
-        app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
+        app.listen(MAIN_PORT, () => console.log('Server is listening on port ' + MAIN_PORT));
     } catch (e) {
         console.log('SERVER ERROR', e);
     }
 };
 
-startApp();
+startApp()
