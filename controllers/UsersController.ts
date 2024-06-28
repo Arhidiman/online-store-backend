d0bbdedeb4248c96d1cbe7f9d9942b4331c05550
Dmitry <arhidiman@mail.ru>
2024-06-28 23:27:08 +0700

    feat: base user creation

--- controllers/UsersController.ts
+++ controllers/UsersController.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from 'express';
 import {usersModel} from "../models/UsersModel.ts";
+import axios, {AxiosResponse} from "axios";
+
+const AUTH_PORT = 6000;
+const BASE_URL = 'http://localhost'
+
 
 class UsersController {
     async create(req: Request, res: Response) {
         try {
-            console.log(req.body)
-            const {username, password} = req.body;
-            await usersModel.createUser(username, password)
-            res.status(201).send('success')
-        } catch (err) {
-            console.log(`Ошибка при создании пользователя ${err}`)
+            const data = req.body
+            console.log(data, 'request from client')
+
+            const authUrl = `${BASE_URL}:${AUTH_PORT}/user/auth`
+            console.log(authUrl, 'auth url')
+
+            const authRes: AxiosResponse = await axios.post(authUrl, data)
+            const authData = authRes.data
+            console.log(authData, 'response from auth service')
+            res.status(201).send('response from auth server completed successfully')
+        } catch (err: any) {
+            console.log(err.message)
         }
     }
 
--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 const express = require('express')
 import {Request, Response} from "express";
 import axios, {AxiosResponse} from 'axios'
-const request = require('request')
 
-// import request from 'request'
 const cors = require('cors')
 import goodsRouter from "./router/goodsRouter";
 import usersRouter from './router/usersRouter';
-//
+
+
 const app = express();
 const MAIN_PORT = 5000;
 const AUTH_PORT = 6000;
-// //
+
+
 app.use(express.json());
 app.use(cors());
 app.use('/api', goodsRouter);
@@ -19,21 +19,6 @@ app.use('/api', usersRouter);
 
 const BASE_URL = 'http://localhost'
 
-
-app.post('/reg', async (req: Request, res: Response) => {
-    try {
-        const data = req.body
-        console.log(data, 'request from client')
-        console.log(`${BASE_URL}:${AUTH_PORT}/reg`)
-        
-        const authRes: AxiosResponse = await axios.post(`${BASE_URL}:${AUTH_PORT}/reg`, data)
-        const authData = authRes.data
-        console.log(authData, 'response from auth service')
-        res.status(201).send('response from auth server completed successfully')
-    } catch (err: any) {
-        console.log(err.message)
-    }
-})
 
 const startApp = async () => {
     try {
